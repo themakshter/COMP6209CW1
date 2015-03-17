16c7f98082defbdbf026b5eb7d5264e4c8ebc9a2
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-03-17 23:51:21 +0000

    Add more tests

--- src/q1/Test.java
+++ src/q1/Test.java
@@ -1,8 +1,17 @@
 package q1;
 
+import q1.subPack.C;
+
 public class Test {
 	public static void main(String[] args) {
+		A a = new A();
+		a.foo(1);
 		B b = new B();
 		b.foo(2);
+		C c = new C();
+		c.foo(3);
+		a.foo(4);
+		b.foo(5);
+		c.foo(6);
 	}
 }
